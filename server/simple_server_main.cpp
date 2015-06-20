aff5cfdb7af380b2c57b26d919b5edf0a0fa61be
kora_hacker <kora.hacker@qq.com>
2015-06-20 15:04:44 +0800

    done server

--- server/simple_server_main.cpp
+++ server/simple_server_main.cpp
@@ -2,19 +2,21 @@
 #include "SocketException.h"
 #include <string>
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main ( int argc, char* argv[] )
 {
-  std::cout << "running....\n";
-
-  try
+	std::cout << "running....\n";
+	
+	try
     {
-      // Create the socket
-      // ServerSocket server ( 30000 );
-	  ServerSocket server (12354);
-
-      while ( true )
-	{
+		// Create the socket
+		// ServerSocket server ( 30000 );
+		ServerSocket server (12354);
+		
+		while ( true )
+		{
 
 	  ServerSocket new_sock;
 	  server.accept ( new_sock );
@@ -26,6 +28,11 @@ int main ( int argc, char* argv[] )
 		  std::string data;
 		  new_sock >> data;
 		  std::cout << data << std::endl;
+
+		  ofstream outfile("/home/kora/桌面/log.txt", ios::app);
+		  outfile << data << endl;
+		  outfile.close();
+
 		  new_sock << data;
 		}
 	    }
